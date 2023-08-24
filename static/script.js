b2ae6126f7e119433bfb053833b38058f2711b4b
Izzy <izzy@email.com>
2023-08-24 14:48:30 +0000

    Updated layout of delete button

--- static/script.js
+++ static/script.js
@@ -113,9 +113,16 @@ fetch('http://localhost:5000/products', {
       productsHtml += '<td>' + products[i][3] + '</td>';
       productsHtml += '<td>' + products[i][4] + '</td>';
       productsHtml += '<td>' + products[i][5] + '</td>';
+      productsHtml += `<td><button class="deleteButton">
+                        <span onclick="deleteHolding();" >CONFIRM DELETE</span>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                          <path stroke-linecap="round" stroke-linejoin="round" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                        </svg>
+                      </button></td>`;
       productsHtml += '</tr>';
     }
-    productsHtml += '</table>'
+    productsHtml += '</table>';
+    
     //id, holdingName, dateOfPurchase, priceAtPurchase, currentPrice, qty
     // display the HTML in the browser
     document.getElementById('products').innerHTML += productsHtml;
